3a6afb085f5362470d873cd250d7dd4a3400dfc3
DT <d.torucki@gmail.com>
2018-03-28 20:46:39 +0200

    1st try

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+                
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+   {
+        [Required(ErrorMessage = "Podaj imię i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Podaj adres e-mail")]
+        [RegularExpression(".+\\@.+\\..", ErrorMessage = "Podaj poprawny adres e-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Podaj numer telefonu")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Będziesz czy nie??")]
+        public bool? WillAttend { get; set; }
+    }
+}
